cedccb989410d677dfb1fbeaad7d16b58a0327ee
HenriqueProen <henriquesgproenca@gmail.com>
2021-04-26 09:54:13 -0300

    Creating routes.

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeGetComponent } from './employee-get/employee-get.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'employee/create', component:EmployeeAddComponent},
+  {path: 'employee/:id', component:EmployeeEditComponent},
+  {path: 'employee', component:EmployeeGetComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
